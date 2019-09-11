ea32afd2357cfab71caa3670afa82ba7867cc177
Adam Mrachek <amrachek@outlook.com>
2019-09-11 19:28:08 +0000

    Added #display_full_poll to show full results

--- lib/polls_2020/cli.rb
+++ lib/polls_2020/cli.rb
@@ -1,6 +1,7 @@
 # The CLI Controller
 
 class Polls2020::CLI
+  attr_accessor :polls
   def call
     list_polls
     menu
@@ -20,13 +21,20 @@ class Polls2020::CLI
     while input != 'exit'
       puts "Enter the number of a poll to see all candidates, type list to see options again, or type exit:"
       input = gets.chomp.downcase
-      if input.to_i > 0 && input.to_i < @polls.size
-        puts @polls[input.to_i - 1].name
+      if input.to_i > 0 && input.to_i <= @polls.size
+        display_full_poll(input.to_i - 1)
       elsif input == 'list'
         list_polls
       else
         puts "Not a valid input. Type list to see options or type exit."
       end
+    end
+  end
+
+  def display_full_poll(selection)
+    puts "#{@polls[selection].name} (#{@polls[selection].date})"
+    @polls[selection].results.each do |result|
+      puts "#{result[0]} - #{result[1]}"
     end
   end
 
--- lib/polls_2020/poll.rb
+++ lib/polls_2020/poll.rb
@@ -34,13 +34,6 @@ class Polls2020::Poll
       polls << new_poll
     end
 
-    # date = poll.css(".date-wrapper").text
-    # name = poll.css(".pollster-container a:nth-child(2)").text
-    # result_name = poll.css(".mobile-answer p").text
-    # result_percent = poll.css(".heat-map").text
-    # all_results = poll.css(".mobile-answer")
-
-    # binding.pry
     polls
   end
 
